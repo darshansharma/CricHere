bb1ea10e2ffd233b3a729eda8fe8008df22d3f9a
Darshan <Darshan@ADMINRG-4RJVQJS>
2017-10-17 21:53:31 +0530

    match Detail part completed (50% done)

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,12 +1,63 @@
 package test;
 
+import java.io.IOException;
 import org.jsoup.*;
-import java.util.concurrent.TimeUnit;
+import org.jsoup.nodes.Document;
+
+class CricAsk{
+    String apikey;
+    public void setApiKey(String apikey){
+        this.apikey = apikey;
+    }
+    public void getMatchDetails() throws IOException{
+        String doc = Jsoup.connect("http://cricapi.com/api/matchCalendar?apikey="+apikey).header("Accept", "text/javascript").ignoreContentType(true).execute().body();
+        System.out.println(doc);
+        String cname = "India"; //Write your country name here
+        String cname1 = cname+" v"; // 1st possiblity of your country in JSON data
+        String cname2 = "v ".concat(cname); // 2nd possiblity of your country name in JSON data        
+        String matchdetail = null;
+        StringBuilder sb = new StringBuilder();
+        char cname_array[] =  new char[10000];
+        int cindex=0; ////set country position from json in local variable
+        int cindex1 = 0; // cindex 1 for cname1 
+        int cindex2 = 0; // cindex 2 for cname2
+        if(doc.toLowerCase().contains(cname1.toLowerCase())){
+            cindex1 = doc.indexOf(cname1); 
+            //System.out.println(cindex1);            
+        }
+        if(doc.toLowerCase().contains(cname2.toLowerCase())){
+            cindex2 = doc.indexOf(cname2); 
+            //System.out.println(cindex2);            
+        }
+        if(cindex1 > cindex2){ 
+            cindex=cindex2; 
+            int tempIndex=0;            
+            tempIndex = doc.indexOf("name", cindex-15);
+            //System.out.println("TEMP INDEX: " + tempIndex);
+            tempIndex += 7;
+            cindex = tempIndex;            
+        }else{ cindex=cindex1; }        
+        for(int i=cindex; i<doc.indexOf("}", cindex); i++){
+            sb.append(doc.charAt(i));            
+        }
+        
+        matchdetail = sb.toString();
+        matchdetail = matchdetail.replaceAll("\"", "");        
+        System.out.println(matchdetail);
+    }
+
+}
+
+
 
 public class Test {
     public static void main(String[] args) throws Exception {
+        String apikey = "0RKpiKRCFQPUYCGiFXA24UvDiRW2";       
+        CricAsk ca = new CricAsk();
+        ca.setApiKey(apikey);
+        ca.getMatchDetails();
        
-     while(true){
+     /*while(true){
         org.jsoup.nodes.Document doc = Jsoup.connect("http://www.cricbuzz.com/").get();
          //System.out.println(doc.toString());
          //Element e = getElementsByClass('gamelist D(f) Fld(r) Pos(a) Trsdu(.3s)');
@@ -38,7 +89,7 @@ public class Test {
          
          TimeUnit.SECONDS.sleep(10);
 
-        }
+        }*/
     }
 }
 
